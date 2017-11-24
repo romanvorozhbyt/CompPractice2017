5cdee136903df15296dcaf08546553a04d96738b
romanvorozhbyt <32297781+romanvorozhbyt@users.noreply.github.com>
2017-11-25 00:21:45 +0200

    Add files via upload

--- Chapter4/Lab 4.5/Lab 4.5.7(5).cpp
+++ Chapter4/Lab 4.5/Lab 4.5.7(5).cpp
@@ -1,125 +1,123 @@
 #include <iostream>
 #include <math.h>
 #include <string>
-using namespace std;
 
-string ForDPatternNLeng(string pattern, string sentence)
+std::string ForDPatternNLeng(std::string pattern, std::string sentence)
 {
-    string result;
-    int k = 0;
-    for(int i = 0; i < pattern.length(); i++)
-        for(int j = k; j < sentence.length(); j++)
-            if(pattern.substr(i, 1) == "D")
-                if((sentence.substr(j, 1).compare(pattern.substr(i, 1)) <= -11) && sentence.substr(j, 1).compare(pattern.substr(i, 1)) >= -20)
-                {
-                    result += sentence.substr(j, 1);
-                    k = j + 1;
-                    j = sentence.length();
-                }
-    return result;
+	std::string result;
+	int k = 0;
+	for (int i = 0; i < pattern.length(); i++)
+		for (int j = k; j < sentence.length(); j++)
+			if (pattern.substr(i, 1) == "D")
+				if ( ((sentence[j] - pattern[i]) <= -11) && sentence[j] - pattern[i] >= -20)
+				{
+					result += sentence.substr(j, 1);
+					k = j + 1;
+					j = sentence.length();
+				}
+	return result;
 }
 
-string ForAPatternNLeng(string pattern, string sentence)
+std::string ForAPatternNLeng(std::string pattern, std::string sentence)
 {
-    string result;
-    for(int j = 0; j < sentence.length(); j++)
-        for(int i = 0; i < pattern.length(); i++)
-            if(result.length() == pattern.length())
-                return result;
-            else
-            {    
-                if(sentence.substr(j, sentence.length()).substr(i, 1).compare("A") >= 0 && sentence.substr(j, sentence.length()).substr(i, 1) != " ")
-                    result += sentence.substr(j, sentence.length())[i];
-                else
-                {
-                    result = "";
-                    break;
-                }
-            }
-    return result;
+	std::string result;
+	for (int j = 0; j < sentence.length(); j++)
+		for (int i = 0; i < pattern.length(); i++)
+			if (result.length() == pattern.length())
+				return result;
+			else
+			{
+				if (sentence.substr(j, sentence.length()).substr(i, 1).compare("A") >= 0 && sentence.substr(j, sentence.length()).substr(i, 1) != " ")
+					result += sentence.substr(j, sentence.length())[i];
+				else
+				{
+					result = "";
+					break;
+				}
+			}
+	return result;
 }
 
-string ForQMPatternNLeng(string pattern, string sentence, string part)
-    int n = sentence.find(part) + part.length();
-    return sentence.substr(n, pattern.length());
+std::string ForQMPatternNLeng(std::string pattern, std::string sentence, std::string part)
+{
+	int n = sentence.find(part) + part.length();
+	return sentence.substr(n, pattern.length());
 }
 
-string ForLCLPatternNLeng(string pattern, string sentence)
+std::string ForLCLPatternNLeng(std::string pattern, std::string sentence)
 {
-    string result;
-    int k = 0;
-    for(int i = 0; i < pattern.length(); i++)
-        for(int j = k; j < sentence.length(); j++)
-            if(sentence[j] == pattern[i] || abs(string(1, sentence[j]).compare(string(1, pattern[i]))) == 32)
-            {   
-                result += sentence[j];
-                i++;
-                if(result.length() == pattern.length())
-                    return result;
-            }
-            else
-            {
-                i = 0;
-                result = "";
-            }
-    return result;
+	std::string result;
+	int k = 0;
+	for (int i = 0; i < pattern.length(); i++)
+		for (int j = k; j < sentence.length(); j++)
+			if (sentence[j] == pattern[i] || abs(sentence[j] - pattern[i]) == 32)
+			{
+				result += sentence[j];
+				i++;
+				if (result.length() == pattern.length())
+					return result;
+			}
+			else
+			{
+				i = 0;
+				result = "";
+			}
+	return result;
 }
 
-string FindAlg(string pattern, string sentence, string part)
+std::string FindAlg(std::string pattern, std::string sentence, std::string part)
 {
-    int value = pattern.substr(0, 1).compare("A");
-    switch(value)
-    {
-        case 0:
-            return ForAPatternNLeng(pattern, sentence);
-        case 3:
-            return ForDPatternNLeng(pattern, sentence);
-        case -2:
-            return ForQMPatternNLeng(pattern, sentence, part);
-        default:
-            return ForLCLPatternNLeng(pattern, sentence);
-    }
+	if(pattern.substr(0, 1)=="A")
+		return ForAPatternNLeng(pattern, sentence);
+	if (pattern.substr(0, 1) == "D")
+		return ForDPatternNLeng(pattern, sentence);
+	if (pattern.substr(0, 1) == "?")
+		return ForQMPatternNLeng(pattern, sentence, part);
+	else
+		return ForLCLPatternNLeng(pattern, sentence);
 }
 int main()
-{   
-    string pattern, sentence, result = "", part;
-    cout << "Input pattern please" << endl;
-    getline(cin, pattern);
-    cout << "Input sentence please" << endl;
-    getline(cin, sentence);
-    int k = 0;
-    int i = 0;
-    cout << "The parts of sentence that matching your pattern is:" << endl;
-    
-    for(int j = 0; j < sentence.length() - pattern.length() + 1; j++)
-    {
-        
-        do
-        {
-            part = pattern.substr(k, i - k + 1);
-            i++;
-        }while(pattern[i] == pattern[k]);
-        k = i;
-        if(FindAlg(part, sentence.substr(j, pattern.length()), result).length() < part.length())
-        {
-            result = "";
-            i = 0;
-            k = 0;
-        }
-        else
-        {
-            result += FindAlg(part, sentence.substr(j, pattern.length()), result);
-            if(result.length() == pattern.length())
-            {
-                if(result == sentence.substr(j, pattern.length()))
-                    cout << result << endl;
-                result = "";
+{
+	std::string pattern, sentence, result = "", part;
+	std::cout << "Input pattern please" << std::endl;
+	getline(std::cin, pattern);
+	std::cout << "Input sentence please" << std::endl;
+	getline(std::cin, sentence);
+	int k = 0;
+	int i = 0;
+	std::cout << "The parts of sentence that matching your pattern is:" << std::endl;
+	
+	for (int j = 0; j < sentence.length() - pattern.length() + 1; j++)
+	{
+		
+		do
+		{
+			part = pattern.substr(k, i - k + 1);
+			i++;
+		} while (pattern[i] == pattern[k]);
+		k = i;
+		if (FindAlg(part, sentence.substr(j, pattern.length()), result).length() < part.length())
+		{
+			
+			result = "";
+			i = 0;
+			k = 0;
+		}
+		else
+		{
+			result += FindAlg(part, sentence.substr(j, pattern.length()), result);
+			if (result.length() == pattern.length())
+			{
+				if (result == sentence.substr(j, pattern.length()))
+					std::cout << result << std::endl;
+				result = ""; 
+							
 				i = 0;
-                k = 0;
-            }
-            else
-                j--;
-        }
-    }
-    return 0;
+				k = 0;
+			}
+			else
+				j--;
+		}
+	}
+	return 0;
 }
